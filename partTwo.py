6dba5b2355d44b38f932689a8888095d731977fe
dylan lewis <djl51@kent.ac.uk>
2026-01-19 14:31:26 +0000

    partTwo.v2.py

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,15 @@
 import math  
 
 def main(): 
-a = int(input("what is a? "))
-b = int(input("what is b? "))
-def pythag(A,B):
-#TO DO  
+    a = int(input("what is a? "))
+    b = int(input("what is b? "))
+    
+    pythag(a,b)
+def pythag(a,b):
+     
+    sa = a**2
+    sb = b**2
+    c = math.sqrt(sa + sb)
+    print(c) 
 
 main()
